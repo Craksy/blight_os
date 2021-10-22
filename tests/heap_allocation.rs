0727099d968b205d7b59bc745c8d1d3527395370
Craksy <amnesia@Amnesia-PC.localdomain>
2021-10-22 20:08:10 +0200

    add test case to heap_allocation integration test

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -10,7 +10,7 @@ extern crate alloc;
 use core::panic::PanicInfo;
 
 use alloc::{boxed::Box, vec::Vec};
-use blight_os::{allocator::HEAP_SIZE, memory::BootInfoFrameAllocator};
+use blight_os::{allocator::HEAP_SIZE, memory::BootInfoFrameAllocator, serial_println};
 use bootloader::{entry_point, BootInfo};
 use x86_64::VirtAddr;
 
@@ -36,7 +36,7 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[test_case]
-fn test_allocation() {
+fn simple_allocation() {
     let value1 = Box::new(42);
     let value2 = Box::new(69);
     assert_eq!(*value1, 42);
@@ -46,18 +46,59 @@ fn test_allocation() {
 #[test_case]
 fn large_allocation() {
     let mut v = Vec::new();
-    let n = 1000;
+    let n = HEAP_SIZE / 32;
     for i in 0..n {
         v.push(i);
     }
-
-    assert_eq!(v.iter().sum::<u64>(), (n - 1) * n / 2);
+    assert_eq!(v.iter().sum::<usize>(), (n - 1) * n / 2);
 }
 
 #[test_case]
-fn reuse_heap_memory() {
+fn reuse_memory() {
     for i in 0..HEAP_SIZE {
         let b = Box::new(i);
         assert_eq!(*b, i);
     }
+}
+
+#[test_case]
+fn reuse_memory_with_longlived() {
+    let long_lived = Box::new(420);
+    for i in 0..HEAP_SIZE {
+        let b = Box::new(i);
+        assert_eq!(*b, i);
+    }
+    assert_eq!(*long_lived, 420);
+}
+
+#[test_case]
+fn space_gets_released() {
+    assert_eq!(
+        blight_os::allocator::ALLOCATOR.lock().get_free_space(),
+        HEAP_SIZE,
+        "Some previous allocations weren't released"
+    );
+}
+
+#[test_case]
+fn reuse_merge() {
+    // split heap in 4 free regions
+    {
+        let _v1 = alloc::vec![1 as u64; HEAP_SIZE / 32];
+        let _v2 = alloc::vec![1 as u64; HEAP_SIZE / 32];
+        let _v3 = alloc::vec![1 as u64; HEAP_SIZE / 32];
+    }
+    // attempt to allocate half of the heap. only works if previously released
+    // regions were merged
+    let require_merge = alloc::vec![1 as u64; HEAP_SIZE/16];
+    assert_eq!(require_merge.iter().sum::<u64>() as usize, HEAP_SIZE / 16);
+}
+
+#[test_case]
+fn empty_when_allocations_dopped() {
+    assert_eq!(
+        blight_os::allocator::ALLOCATOR.lock().get_length(),
+        2,
+        "Heap not empty"
+    )
 }
